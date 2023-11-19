81f92cc6236f9b3b890d9e7bcc9ada4701729cf4
RichardGarita <richard.garita@ucr.ac.cr>
2023-11-19 15:50:21 -0600

    Act

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    VITE_BASE_URL: string;
+    VITE_API_URL: string;
+    VITE_APP_MODE: string;
+    // ... otras variables de entorno específicas de Vite
+  }
